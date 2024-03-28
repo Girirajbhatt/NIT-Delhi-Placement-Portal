f258e605f02c1d35795ea2539c96262d1660802a
Girirajbhatt <giriraj12a@gmail.com>
2024-03-28 15:17:22 +0530

    added index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/' , (req,res) => {
     res.send("This is my home page");
 })
 
-app.listen(process.env.PORT, ()=>{
+app.listen(port, ()=>{
     console.log(`App is listening on port : ${port}`)
 })
