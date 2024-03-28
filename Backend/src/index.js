58687762ff7652f77df4f5056337d3af14236e48
Girirajbhatt <giriraj12a@gmail.com>
2024-03-28 21:43:51 +0530

    added utils code and api response

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    try {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(`Server connection failed : `,error);
+    }
+})
+.catch((err) => {
+    console.log("MongoDB connection failed : ",err);
+})
